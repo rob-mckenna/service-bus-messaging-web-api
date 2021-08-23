8a148ed0118fc86a6d335354891597dac85acb5a
Rob McKenna <14824673+rob-mckenna@users.noreply.github.com>
2021-08-22 22:18:27 -0400

    Add Health Check

---
+++ service-bus-messaging-web-api/Controllers/HealthCheckController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace service_bus_messaging_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Healthy";
+        }
+    }
+}
